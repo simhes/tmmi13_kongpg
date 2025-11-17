b9b60aeafb7e86a2e8e654ef7bd6fcc7e8e525ce
Simon Hesselstrand <simon.hesselstrand@outlook.com>
2025-11-17 13:13:37 +0100

    (M) updated for correct arm movment, cylender calaculations

--- matlab/cylinder_fource.m
+++ matlab/cylinder_fource.m
@@ -1,6 +1,6 @@
 function cyl_fource = cylinder_fource (th_1, th_2, l1, l2, F, e)
     % cyl 1
-    ar1_x = cos(th_1) * l1 + cos(th_1 + th_2) * l2;
+    ar1_x = cos(th_1) * l1 + cos(th_2) * l2;
     mt1 = ar1_x * F;
     % mt = cyl_m = e * cyl_f
     cyl_fource = mt1 ./ e;
--- matlab/main_1.m
+++ matlab/main_1.m
@@ -8,7 +8,7 @@ t = 1:1:length(th_1);
 
 %% pozitions plot
 % plot z
-z = L_0 + sin(th_1) .* l1 + sin(th_1 + th_2) .* l2;
+z = L_0 + sin(th_1) .* l1 + sin(th_2) .* l2;
 disp('Figure 1, Crane hite over time')
 figure(1)
 subplot(2,2,1)
@@ -20,7 +20,7 @@ ylabel('Higth [m]')
 grid on
 
 % plot x
-x = cos(th_1) .* l1 + cos(th_1 + th_2) .* l2;
+x = cos(th_1) .* l1 + cos(th_2) .* l2;
 disp('Figure 2, Crane horizonal reach')
 
 %figure(2)
@@ -39,7 +39,8 @@ F = mt * g;
 cyl1_f = cylinder_fource(th_1, th_2, l1, l2, F, e_1);
 disp('Force on cylinder 1')
 
-subplot(2,2,[3,4])
+% subplot(2,2,[3,4])
+subplot(2,2,3)
 
 plot(t,cyl1_f)
 title('Figure 3, Force on cylinder 1')
@@ -58,6 +59,17 @@ xlabel('Time [s]')
 ylabel('Fource [N]')
 legend('Cyl1', 'Cyl2')
 hold off
+
+% plot 3d
+subplot(2,2,4)
+
+plot3(t,x,z)
+title('T, X, Z')
+grid on
+xlabel('Time')
+ylabel('Reach [m]')
+zlabel('Hegth [m]')
+
 %% wors case
 % th_#_wc_cyl#
 F_max = g * mt_max;
@@ -78,4 +90,21 @@ disp(   '+======================+')
 fprintf('| wc cyl2:             |\n')
 fprintf('|   cyl1: %d |\n', cyl_1_wc2)
 fprintf('| * cyl2: %d |\n', cyl_2_wc2)
+disp(   '+======================+')
+
+%% cyl deameter
+
+% ps_max * 10^5 * A = F
+
+
+cyl1_a = cyl_1_wc1 / ( ps_max * 10^5);
+cyl2_a = cyl_2_wc2 / ( ps_max * 10^5);
+
+cyl1_d = 2 * sqrt( cyl1_a/pi );
+cyl2_d = 2 * sqrt( cyl2_a/pi );
+
+disp('Cylinder diamters (wc): ')
+disp(   '+======================+')
+fprintf('| cyl1_d: %d |\n', cyl1_d)
+fprintf('| cyl2_d: %d |\n', cyl2_d)
 disp(   '+======================+')
